e37db9f6a207a331088eaf81334d369ed45d71b6
shadowofzeus <shadowofzeus811@gmail.com>
2020-02-24 16:34:13 +0200

    Base Game Play done

---
+++ app.js
@@ -0,0 +1,72 @@
+//console.log('VSCODE!!!');
+
+/*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+/*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+let win;
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('reset-button').addEventListener('click', init);
+/*----- functions -----*/
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+};
+    //be sure to call the init function!
+
+    function render() {
+        board.forEach(function(val, idx) {
+        squares[idx].textContent = val;
+        });
+        // new code below
+        messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+        };
+
+    function handleTurn() {
+        let idx = squares.findIndex(function(square) {
+        return square === event.target;
+        });
+        board[idx] = turn;
+        // This is tidy
+        turn = turn === 'X' ? 'O' : 'X';
+        // In an if statement it would look like: 
+        // if (turn === 'X') {
+        // turn = 'O' 
+        // } else {
+        // turn = 'X' 
+        // };
+        // writing the ternary saved you from all that. 
+        win = getWinner();
+        render();
+        };
+
+        function getWinner() {
+            let winner = null;
+            winningCombos.forEach((combo, index) => {
+            if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+            winner = board[combo[0]];
+            }
+            });
+            // new code below
+            return winner ? winner : board.includes('') ? null : 'T';
+        };
+
+        init();
+        render();
